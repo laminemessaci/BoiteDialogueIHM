9490c39f6094fddd49380f92d066214fd0b47966
lamine messaci <lamine.messaci18@gmail.com>
2020-03-21 12:20:14 +0100

    Class Fenetre

---
+++ src/com/lamine/Fenetre.java
@@ -0,0 +1,27 @@
+package com.lamine;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Fenetre  extends JFrame {
+    private JButton button = new JButton("Appelà la BoiteDialogue");
+
+    public Fenetre() {
+
+        this.setTitle("Ma JFrame");
+        this.setSize(300,300);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.getContentPane().setLayout(new FlowLayout());
+        this.getContentPane().add(button);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                BoiteDialog bd = new BoiteDialog(null, "Coucou", true);
+            }
+        });
+        this.setVisible(true);
+    }
+}
--- src/com/lamine/Main.java
+++ src/com/lamine/Main.java
@@ -3,6 +3,7 @@ package com.lamine;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        new Fenetre();
     }
 }
